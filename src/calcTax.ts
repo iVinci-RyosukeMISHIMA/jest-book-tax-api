2121e8b0a2145198947e98ca14e0974bb10e9870
iVinci-RyosukeMISHIMA <ryosuke.mishima@i-vinci.co.jp>
2024-02-29 10:39:14 +0900

    課税対象金額算出メソッド、及びテストコード実装

--- src/calcTax.ts
+++ src/calcTax.ts
@@ -5,7 +5,14 @@ type calcRetirementIncomeDeductionInput = {
         isDisabled: boolean
 } 
 
-const calcRetirementIncomeDeduction = (input: calcRetirementIncomeDeductionInput) => {
+type calcTaxationTargetAmountInput = {
+        yearsServiced: number,
+        retirementIncome: number,
+        isExecutiveOfficer: boolean,
+        isDisabled: boolean
+}
+
+export const calcRetirementIncomeDeduction = (input: calcRetirementIncomeDeductionInput) => {
     let deduction;
     const deductionUntil20years = 400_000;
     const deductionAfter20years = 700_000;
@@ -21,4 +28,32 @@ const calcRetirementIncomeDeduction = (input: calcRetirementIncomeDeductionInput
     return input.isDisabled ? deduction + extraDeductionIfDisabled : deduction;
 }
 
-export default calcRetirementIncomeDeduction;
+//課税対象金額算出
+export const calcTaxationTargetAmount = (input: calcTaxationTargetAmountInput) => {
+    //対象金額
+    let targetAmount;
+    //退職所得 - 控除額
+    let diffIncomeAndDeduction = input.retirementIncome - calcRetirementIncomeDeduction({ yearsServiced: input.yearsServiced, isDisabled: input.isDisabled })
+    //(退職所得 - 控除額) <= 0円の場合
+    if (diffIncomeAndDeduction <= 0) {
+        return 0;
+    }
+    //勤続年数5年以下の場合
+    if (input.yearsServiced <= 5) {
+        //役員等の場合
+        if (input.isExecutiveOfficer === true) {
+            targetAmount = diffIncomeAndDeduction;
+        //役員等以外、かつ(退職所得 - 控除額) > 300万円 の場合
+        } else if (input.isExecutiveOfficer === false && diffIncomeAndDeduction > 300_0000) {
+            targetAmount = 300_0000 / 2 + (diffIncomeAndDeduction - 300_0000)
+        //役員等以外、かつ(退職所得 - 控除額) <= 300万円 の場合
+        } else
+            targetAmount = diffIncomeAndDeduction / 2;
+    //勤続年数5年を超える場合
+    } else {
+        targetAmount = diffIncomeAndDeduction / 2;
+    }
+    //1000円未満を切り捨て
+    targetAmount = Math.floor(targetAmount / 1000) * 1000;
+    return targetAmount;
+} 
--- src/test/calcTax.test.ts
+++ src/test/calcTax.test.ts
@@ -1,6 +1,6 @@
-import calcRetirementIncomeDeduction from "../calcTax";
+import { calcRetirementIncomeDeduction, calcTaxationTargetAmount } from "../calcTax"
 
-describe("退職所得控除額",()=> {
+describe("退職所得控除額 = 勤続年数（20年以前）* 控除額（20年以前） + 勤続年数（21年目以降）* 控除額（21年目以降） + 障害補填（障害が起因の場合）",()=> {
     test.each`
     yearsServiced | isDisabled | expected
     ${1}          | ${false}   | ${800_000}
@@ -10,6 +10,7 @@ describe("退職所得控除額",()=> {
     ${20}          | ${false}   | ${8000_000}
     ${21}          | ${false}   | ${8700_000}
     ${30}          | ${false}   | ${15000_000}
+    
     ${1}          | ${true}   | ${1800_000}
     ${2}          | ${true}   | ${1800_000}
     ${3}          | ${true}   | ${2200_000}
@@ -22,4 +23,62 @@ describe("退職所得控除額",()=> {
     expect(deduction).toBe(expected)
     }
 )
+})
+
+describe("課税対象金額", () => {
+    test.each`
+    yearsServiced | retirementIncome | isExecutiveOfficer | isDisabled | expected
+    ${1} | ${2990_300} | ${false} | ${false} | ${1095000}
+    ${1} | ${3000_000} | ${false} | ${false} | ${1100000}
+    ${1} | ${3101_000} | ${false} | ${false} | ${1150000}
+    ${1} | ${5010_000} | ${false} | ${false} | ${2710000}
+    
+    ${1} | ${2990_300} | ${true} | ${false} | ${2190000}
+    ${1} | ${3000_000} | ${true} | ${false} | ${2200000}
+    ${1} | ${3101_000} | ${true} | ${false} | ${2301000}
+    ${1} | ${5010_000} | ${true} | ${false} | ${4210000}
+    
+    ${4} | ${2990_300} | ${false} | ${false} | ${695000}
+    ${4} | ${3000_000} | ${false} | ${false} | ${700000}
+    ${4} | ${3101_000} | ${false} | ${false} | ${750000}
+    ${4} | ${5010_000} | ${false} | ${false} | ${1910000}
+
+    ${4} | ${2990_300} | ${true} | ${false} | ${1390000}
+    ${4} | ${3000_000} | ${true} | ${false} | ${1400000}
+    ${4} | ${3101_000} | ${true} | ${false} | ${1501000}
+    ${4} | ${5010_000} | ${true} | ${false} | ${3410000}
+
+    ${5} | ${2990_300} | ${false} | ${false} | ${495000}
+    ${5} | ${3000_000} | ${false} | ${false} | ${500000}
+    ${5} | ${3101_000} | ${false} | ${false} | ${550000}
+    ${5} | ${5010_000} | ${false} | ${false} | ${1510000}
+
+    ${5} | ${2990_300} | ${true} | ${false} | ${990000}
+    ${5} | ${3000_000} | ${true} | ${false} | ${1000000}
+    ${5} | ${3101_000} | ${true} | ${false} | ${1101000}
+    ${5} | ${5010_000} | ${true} | ${false} | ${3010000}
+
+    ${6} | ${2990_300} | ${false} | ${false} | ${295000}
+    ${6} | ${3000_000} | ${false} | ${false} | ${300000}
+    ${6} | ${3101_000} | ${false} | ${false} | ${350000}
+    ${6} | ${5010_000} | ${false} | ${false} | ${1305000}
+
+    ${6} | ${2990_300} | ${true} | ${false} | ${295000}
+    ${6} | ${3000_000} | ${true} | ${false} | ${300000}
+    ${6} | ${3101_000} | ${true} | ${false} | ${350000}
+    ${6} | ${5010_000} | ${true} | ${false} | ${1305000}
+
+    ${10} | ${2990_300} | ${false} | ${false} | ${0}
+    ${10} | ${3000_000} | ${false} | ${false} | ${0}
+    ${10} | ${3101_000} | ${false} | ${false} | ${0}
+    ${10} | ${5010_000} | ${false} | ${false} | ${505000}
+
+    ${10} | ${2990_300} | ${true} | ${false} | ${0}
+    ${10} | ${3000_000} | ${true} | ${false} | ${0}
+    ${10} | ${3101_000} | ${true} | ${false} | ${0}
+    ${10} | ${5010_000} | ${true} | ${false} | ${505000}
+    `(`勤続年数：$yearsServiced, 退職所得：$retirementIncome, 役員等：$isExecutiveOfficer 障害が起因：$isDisabled, 期待値：$expected`, ({ yearsServiced, retirementIncome, isExecutiveOfficer,isDisabled, expected }) => {
+        const amount = calcTaxationTargetAmount({ yearsServiced, retirementIncome, isExecutiveOfficer, isDisabled})
+        expect(amount).toBe(expected);
+    })
 })
